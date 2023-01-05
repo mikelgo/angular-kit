7f817ca623479c2f14b323e62b89ce8ef791dee1
Michael Berger <m.gonser@icloud.com>
2023-01-05 07:20:18 +0100

    feat(types): add types lib

---
+++ libs/angular-kit/types/README.md
@@ -0,0 +1,18 @@
+# @code-workers-io/angular-kit/types
+
+Tired of writing e.g. `@Input() arg: string | null | undefined` over and over again when using Angular's strict mode?
+
+`@code-workers-io/angular-kit/types` to the rescue! This handy lib provides a collection of types which will reflect strict inputs.
+
+Besides that it offers some coercion helpers to make your life easier.
+
+## Supported types
+
+- `StringType`
+- `NumberType`
+- `BooleanType`
+- `DateType`
+- `ArrayType`
+- `ObjectType`
+- `Nullable<T>`
+- `Optional<T>`
---
+++ libs/angular-kit/types/src/lib/types.ts
@@ -0,0 +1,21 @@
+/** Type describing the allowed values for a boolean Type. */
+export type BooleanType = string | boolean | null | undefined;
+
+/** Type describing the allowed values for a string Type. */
+export type StringType = string | null | undefined;
+
+/** Type describing the allowed values for a number Type. */
+export type NumberType = number | null | undefined;
+
+/** Type describing the allowed values for a date Type. */
+export type DateType = Date | string | number | null | undefined;
+
+/** Type describing the allowed values for an array Type. */
+export type ArrayType<T> = T[] | null | undefined;
+
+/** Type describing the allowed values for an object Type. */
+export type ObjectType<T> = T | null | undefined;
+
+export type Nullable<T> = T | null;
+
+export type Optional<T> = T | null | undefined;
