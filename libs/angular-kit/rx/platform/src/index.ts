f75f5b0fec08dcbb3499b5ee534d98f9c011299c
Michael Berger <m.gonser@icloud.com>
2023-01-10 20:25:38 +0100

    feat(rx/platform): add mutation-observer

--- libs/angular-kit/rx/platform/src/index.ts
+++ libs/angular-kit/rx/platform/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/create-resize-observer';
+export * from './lib/create-intersection-observer';
+export * from './lib/create-mutation-observer';
--- libs/angular-kit/testing/rx/platform.testing.ts
+++ libs/angular-kit/testing/rx/platform.testing.ts
@@ -11,6 +11,33 @@ export function mockResizeObserver() {
   };
 }
 
+export function mockMutationObserver() {
+  window.MutationObserver = class {
+    observe = jest.fn();
+    disconnect = jest.fn();
+    takeRecords = jest.fn();
+    constructor(callback: any) {
+      callback([{ target: { clientWidth: 100, clientHeight: 100 } }]);
+    }
+  };
+}
+export function mockIntersectionObserver() {
+  window.IntersectionObserver = class {
+    observe = jest.fn();
+    disconnect = jest.fn();
+    unobserve = jest.fn();
+    rootMargin = '';
+    root: Element | null = null;
+    thresholds: number[] = [];
+    takeRecords() {
+      return [];
+    }
+    constructor(callback: any) {
+      callback([{ isIntersecting: true }]);
+    }
+  };
+}
+
 export function createElementRef(): ElementRef {
   return {
     nativeElement: document.createElement('div'),
