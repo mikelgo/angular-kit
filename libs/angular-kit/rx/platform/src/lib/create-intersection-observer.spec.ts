431b0c80df719c511e071b77a8079f23d05c0f38
Michael Berger <m.gonser@icloud.com>
2023-01-10 20:20:55 +0100

    feat(rx/platform): add intersection-observer

---
+++ libs/angular-kit/rx/platform/src/lib/create-intersection-observer.spec.ts
@@ -0,0 +1,26 @@
+import { createElementRef, mockIntersectionObserver } from '../../../../testing/rx/platform.testing';
+import { createIntersectionObserver } from './create-intersection-observer';
+import { subscribeSpyTo } from '@hirez_io/observer-spy';
+import { fakeAsync, tick } from '@angular/core/testing';
+
+describe('createIntersectionObserver', () => {
+  describe('supported', () => {
+    beforeEach(() => mockIntersectionObserver());
+    it('should create', () => {
+      const observer = createIntersectionObserver(createElementRef());
+      expect(observer).toBeTruthy();
+    });
+
+    it('should emit on intersect', fakeAsync(() => {
+      const elementRef = createElementRef();
+      const observer = createIntersectionObserver(elementRef);
+
+      const result = subscribeSpyTo(observer);
+      elementRef.nativeElement.dispatchEvent(new Event('intersect'));
+
+      tick(1000);
+
+      expect(result.getValues().length).toEqual(1);
+    }));
+  });
+});
