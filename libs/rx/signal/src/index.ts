9fa3924b246535b8ece36fde4a1e02cf3cc41728
Michael Berger <m.gonser@icloud.com>
2023-01-14 15:29:23 +0100

    feat(signals) add create-signals

--- libs/rx/signal/src/index.ts
+++ libs/rx/signal/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/create-signal';
+export {createSignals} from './lib/create-signals/create-signals'
+export {RxSignals, SignalObservables, SignalDispatchFn, SignalDispatchers, SignalTransformation} from './lib/create-signals/types'
