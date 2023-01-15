1da1d379e6be45ab595568dbd6cec7a91875e8a3
Michael Berger <m.gonser@icloud.com>
2023-01-15 14:18:48 +0100

    chore(cdk): release version 1.0.1

--- libs/rx/platform/README.md
+++
@@ -1,3 +0,0 @@
-# @code-workers.io/angular-kit/rx/platform
-
-A set of reactive helpers wrapping browser APIs.
--- libs/rx/platform/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './lib/create-resize-observer';
-export * from './lib/create-intersection-observer';
-export * from './lib/create-mutation-observer';
-
-export * from './lib/directives/observe-resize.directive';
-export * from './lib/directives/observe-intersection.directive';
