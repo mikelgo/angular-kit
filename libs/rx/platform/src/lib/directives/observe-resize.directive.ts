4fee03821a0207d9290b17826cd4ada3444c197d
Michael Berger <m.gonser@icloud.com>
2023-01-15 14:42:19 +0100

    feat(rx): add directive which uses resize-observer

---
+++ libs/rx/platform/src/lib/directives/observe-resize.directive.ts
@@ -0,0 +1,41 @@
+import {Directive, ElementRef, Input, NgModule, OnDestroy, Output} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Subscription} from 'rxjs';
+import {createResizeObserver, ResizeObserverConfig} from '../create-resize-observer';
+import {createSignal} from '@code-workers.io/angular-kit/rx/signal';
+import {Nullable} from '@code-workers.io/angular-kit/cdk/types';
+
+@Directive({
+  selector: '[observeResize]',
+})
+export class ObserveResizeDirective implements OnDestroy {
+  private subscription = new Subscription();
+  private configSignal = createSignal<ResizeObserverConfig | null>();
+
+  @Input() set resizeObserverConfig(config: ResizeObserverConfig | null) {
+    this.configSignal.send(config);
+  }
+
+  private resizeObserver$ = createResizeObserver(this.element);
+
+  @Output() resizeEvent = this.resizeObserver$;
+
+  constructor(private element: ElementRef) {
+    this.subscription.add(
+      this.configSignal.$.subscribe((config: Nullable<ResizeObserverConfig>) => {
+        this.resizeObserver$ = createResizeObserver(this.element, config ?? undefined);
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+}
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ObserveResizeDirective],
+  exports: [ObserveResizeDirective],
+})
+export class ObserveResizeDirectiveModule {}
