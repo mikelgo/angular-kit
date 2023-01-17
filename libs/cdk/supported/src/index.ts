715987f616b9dd7d8439a26c1ff720d2ca1e8ec9
Michael Berger <m.gonser@icloud.com>
2023-01-17 06:59:27 +0100

    chore(cdk): release version 1.1.0

---
+++ libs/cdk/supported/src/index.ts
@@ -0,0 +1,13 @@
+export * from './lib/supported.module';
+
+/**
+ * TODO
+ *
+ * Token for
+ *
+ * window
+ * util to create general supportsFunctions
+ * supportsResizeObserver
+ * supportsIntersectionObserver
+ * supportsMutationObserver
+ */
