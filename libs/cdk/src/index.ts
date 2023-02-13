17c7369319b3e0f4aea536c4dbfad4feae3c76f4
Michael Berger <m.gonser@icloud.com>
2023-02-13 16:23:43 +0100

    fix(cdk): remove cdkModule
    closes #24

--- libs/cdk/src/index.ts
+++ libs/cdk/src/index.ts
@@ -1,1 +1,3 @@
-export * from './lib/cdk.module';
+// Workaround for: https://github.com/microsoft/rushstack/issues/2806.
+// This is a private export that can be removed at any time.
+export const ɵɵtsModuleIndicatorApiExtractorWorkaround = true;
