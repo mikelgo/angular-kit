b16e2dd1ed26b0e541953f9b605fd6b8134df0c3
Michael Be <m.gonser@icloud.com>
2023-02-15 15:41:11 +0100

    rx: enhance createEffect tests

--- libs/rx/effect/src/lib/create-effect/create-effect.spec.ts
+++ libs/rx/effect/src/lib/create-effect/create-effect.spec.ts
@@ -1,6 +1,6 @@
-import {createEffect} from "./create-effect";
-import {of, Subject} from "rxjs";
-import {subscribeSpyTo} from "@hirez_io/observer-spy";
+import { createEffect } from './create-effect';
+import { Observable, of, Subject } from 'rxjs';
+import { subscribeSpyTo } from '@hirez_io/observer-spy';
 
 describe('createEffect', () => {
   it('should create', () => {
@@ -14,16 +14,27 @@ describe('createEffect', () => {
 
     const effect = createEffect(() => source$, trigger$$);
 
-    const result = subscribeSpyTo(effect)
+    const result = subscribeSpyTo(effect);
 
     trigger$$.next(void 0);
     trigger$$.next(void 0);
 
-
-
-    expect(result.getValues()).toEqual([
-      10,
-      10
-    ]);
+    expect(result.getValues()).toEqual([10, 10]);
   });
-})
+
+  it('should use the argument', () => {
+    const trigger = new Subject<number>();
+
+    const get = (id: number): Observable<number> => {
+      return of(id);
+    };
+
+    const effect = createEffect((id: number) => get(id), trigger);
+    const result = subscribeSpyTo(effect);
+
+    trigger.next(10);
+    trigger.next(42);
+
+    expect(result.getValues()).toEqual([10, 42]);
+  });
+});
