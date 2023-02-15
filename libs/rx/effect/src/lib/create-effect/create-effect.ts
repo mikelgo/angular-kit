ee0e4faee8ebe30912c31f0a6b2b07feb04dcbed
Michael Be <m.gonser@icloud.com>
2023-02-15 15:40:33 +0100

    feat(rx): createEffect supports now callback arguments

--- libs/rx/effect/src/lib/create-effect/create-effect.ts
+++ libs/rx/effect/src/lib/create-effect/create-effect.ts
@@ -1,12 +1,19 @@
-import {concatMap, Observable, Subject} from "rxjs";
+import { concatMap, Observable, Subject } from 'rxjs';
 
 /**
  * Creates an effect from {@param effectSource }that emits when the {@param effectTrigger} is emitted.
  * @param effectSource
  * @param effectTrigger
  */
-export function createEffect<T>(effectSource: () => Observable<T>, effectTrigger: Subject<unknown>): Observable<T> {
-  return effectTrigger.asObservable().pipe(
-    concatMap(() => effectSource())
-  )
+
+export function createEffect<T>(effectSource: () => Observable<T>, effectTrigger: Subject<any>): Observable<T>;
+export function createEffect<T, Targ>(
+  effectSource: (arg: Targ) => Observable<T>,
+  effectTrigger: Subject<any>
+): Observable<T>;
+export function createEffect<T, Targ>(
+  effectSource: (arg?: Targ) => Observable<T>,
+  effectTrigger: Subject<any>
+): Observable<T> {
+  return effectTrigger.asObservable().pipe(concatMap((arg: Targ) => effectSource(arg)));
 }
