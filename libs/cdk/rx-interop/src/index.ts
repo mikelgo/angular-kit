2cbb13f1aa3642051eb461bd49f967ae705539a5
Michael Berger <m.gonser@icloud.com>
2023-04-18 05:49:39 +0200

    fix(cdk): export onChanges correctly

--- libs/cdk/rx-interop/src/index.ts
+++ libs/cdk/rx-interop/src/index.ts
@@ -1,1 +1,2 @@
-export const greeting = 'Hello World!';
+export {TypedSimpleChanges, OnChangesEffect, effectOnChanges$} from './lib/on-changes/effect-on-changes$';
+export * from './lib/on-changes/on-changes-state';
