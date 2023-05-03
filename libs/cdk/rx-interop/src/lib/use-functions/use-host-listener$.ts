13e78909f2a57e9142f8c37caf713e511e32aad5
Michael Berger <m.gonser@icloud.com>
2023-05-03 07:56:17 +0200

    feat(cdk): make it configurable to run change detection of not in useHostListener$

--- libs/cdk/rx-interop/src/lib/use-functions/use-host-listener$.ts
+++ libs/cdk/rx-interop/src/lib/use-functions/use-host-listener$.ts
@@ -1,6 +1,9 @@
-import {ChangeDetectorRef, ElementRef, inject, NgZone, ViewRef} from "@angular/core";
-import {distinctUntilChanged, fromEvent, Observable, ReplaySubject, takeUntil} from "rxjs";
+import {ChangeDetectorRef, ElementRef, inject, NgZone, ViewRef} from '@angular/core';
+import {distinctUntilChanged, fromEvent, Observable, ReplaySubject, takeUntil} from 'rxjs';
 
+export interface UseHostListenerConfig {
+  zoneless?: boolean;
+}
 /**
  *
  * @publicApi
@@ -17,25 +20,25 @@ import {distinctUntilChanged, fromEvent, Observable, ReplaySubject, takeUntil} f
  *
  * @param eventName
  */
-export function useHostListener$<T extends Event>(eventName: string): Observable<T>{
-  const {nativeElement} = inject(ElementRef);
+export function useHostListener$<T extends Event>(eventName: string): Observable<T>;
+export function useHostListener$<T extends Event>(eventName: string, cfg: UseHostListenerConfig): Observable<T>;
+export function useHostListener$<T extends Event>(eventName: string, cfg?: UseHostListenerConfig): Observable<T> {
+  const { nativeElement } = inject(ElementRef);
   const ngZone = inject(NgZone);
+  const cdr = cfg?.zoneless ? undefined : inject(ChangeDetectorRef);
 
   const events$ = new ReplaySubject<T>(1);
 
   ngZone.runOutsideAngular(() => {
     fromEvent<T>(nativeElement, eventName)
-      .pipe(
-        takeUntil(useOnDestroy())
-      )
-      .subscribe(value =>
-        events$.next(value)
-      );
-  })
+      .pipe(takeUntil(useOnDestroy()))
+      .subscribe((value) => {
+        events$.next(value);
+        cfg?.zoneless ? void 0 : cdr.detectChanges();
+      });
+  });
 
-  return events$.asObservable().pipe(
-    distinctUntilChanged()
-  );
+  return events$.asObservable().pipe(distinctUntilChanged());
 }
 
 /**
@@ -50,7 +53,7 @@ export function useOnDestroy() {
   viewRef?.onDestroy(() => {
     onDestroy$.next(void 0);
     onDestroy$.complete();
-  })
+  });
 
   return onDestroy$;
 }
