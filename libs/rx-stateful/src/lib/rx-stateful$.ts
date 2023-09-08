06ee56bff315a7ef8b167db2e5e645fda850b570
Michael Berger <m.gonser@icloud.com>
2023-09-08 08:13:07 +0200

    refactor(rx-stateful): make code more readable

--- libs/rx-stateful/src/lib/rx-stateful$.ts
+++ libs/rx-stateful/src/lib/rx-stateful$.ts
@@ -67,7 +67,11 @@ function createState$<T, E>(source$: Observable<T>, mergedConfig: RxStatefulConf
   const accumulationFn = mergedConfig.accumulationFn ?? defaultAccumulationFn;
   const error$$ = new Subject<RxStatefulWithError<T, E>>();
   const refresh$ = mergedConfig?.refreshTrigger$ ?? new Subject<unknown>();
-  const { request$, refreshedRequest$ } = initSources(source$, error$$, refresh$, mergedConfig);
+
+  const sharedSource$ = initSharedSource(source$, error$$, mergedConfig);
+  const request$: Observable<Partial<InternalRxState<T, E>>> = requestSource(sharedSource$);
+  const refreshedRequest$: Observable<Partial<InternalRxState<T, E>>> = refreshedRequestSource(sharedSource$, refresh$, mergedConfig);
+
 
   return merge(request$, refreshedRequest$, error$$).pipe(
     scan(accumulationFn, {
@@ -88,7 +92,7 @@ function createState$<T, E>(source$: Observable<T>, mergedConfig: RxStatefulConf
   );
 }
 
-function initSource<T, E>(
+function initSharedSource<T, E>(
   source$: Observable<T>,
   error$$: Subject<RxStatefulWithError<T, E>>,
   mergedConfig: RxStatefulConfig<T, E>
@@ -109,20 +113,6 @@ function initSource<T, E>(
   );
 }
 
-function initSources<T, E>(
-  source$: Observable<T>,
-  error$$: Subject<RxStatefulWithError<T, E>>,
-  refresh$: Subject<any>,
-  mergedConfig: RxStatefulConfig<T, E>
-) {
-  const sharedSource$ = initSource<T, E>(source$, error$$, mergedConfig);
-
-  const request$ = requestSource<T, E>(sharedSource$);
-
-  const refreshedRequest$ = refreshedRequestSource<T, E>(sharedSource$, refresh$, mergedConfig);
-
-  return { request$, refreshedRequest$ };
-}
 
 function requestSource<T, E>(source$: Observable<T>): Observable<Partial<InternalRxState<T, E>>> {
   return source$.pipe(
