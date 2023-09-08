a5060b70896d632139feea772fae3ccc8b9bf83f
Michael Berger <m.gonser@icloud.com>
2023-09-08 11:44:15 +0200

    feat(rx-stateful): enhance RxStatefulClient config with AutoRefetchStrategy

--- libs/rx-stateful/src/lib/client/rx-stateful-client/rx-stateful-client.service.ts
+++ libs/rx-stateful/src/lib/client/rx-stateful-client/rx-stateful-client.service.ts
@@ -1,27 +1,29 @@
 import {Injectable} from '@angular/core';
-import {Config, injectRxStatefulConfig} from "../config/rx-stateful-config.provider";
-import {Observable} from "rxjs";
-import {RxStateful, RxStatefulConfig} from "../../types/types";
-import {rxStateful$} from "../../rx-stateful$";
+import {Config, injectRxStatefulConfig} from '../config/rx-stateful-config.provider';
+import {Observable} from 'rxjs';
+import {RxStateful, RxStatefulConfig} from '../../types/types';
+import {rxStateful$} from '../../rx-stateful$';
 
 export type RxStatefulRequestOptions<T, E> = RxStatefulConfig<T, E>;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RxStatefulClient {
-
   private readonly config = injectRxStatefulConfig();
 
-  request<T,E>(source$: Observable<T>): RxStateful<T, E>
-  request<T,E>(source$: Observable<T>, options: RxStatefulRequestOptions<T, E>): RxStateful<T, E>
-  request<T,E>(source$: Observable<T>, options?: RxStatefulRequestOptions<T, E>): RxStateful<T, E>{
+  request<T, E>(source$: Observable<T>): RxStateful<T, E>;
+  request<T, E>(source$: Observable<T>, options: RxStatefulRequestOptions<T, E>): RxStateful<T, E>;
+  request<T, E>(source$: Observable<T>, options?: RxStatefulRequestOptions<T, E>): RxStateful<T, E> {
+
     const mergedConfig = {
-      ...this.config as Config<T, E>,
-        ...options
+      ...(this.config as Config<T, E>),
+      ...options,
+    };
+    if (this.config?.periodicRefetch){
+      mergedConfig?.refetchStrategies?.push(this.config?.periodicRefetch);
     }
 
-        return rxStateful$<T, E>(source$, mergedConfig);
-
+    return rxStateful$<T, E>(source$, mergedConfig);
   }
 }
