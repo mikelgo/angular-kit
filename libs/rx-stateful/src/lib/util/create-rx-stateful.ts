bba07030892e47bf6ca7d5b0c8ce85bd9c62c4a3
Michael Berger <m.gonser@icloud.com>
2023-10-25 07:57:29 +0200

    chore(rx-stateful): remove commented out code

--- libs/rx-stateful/src/lib/util/create-rx-stateful.ts
+++ libs/rx-stateful/src/lib/util/create-rx-stateful.ts
@@ -37,38 +37,4 @@ export function createRxStateful<T, E>(state$: Observable<InternalRxState<T, E>>
         filter((value) => value !== undefined),
         distinctUntilChanged()
     )
-  // return {
-  //   value$: state$.pipe(
-  //     map((state, index) => {
-  //       /**
-  //        * todo there is for sure a nicer way to do this.
-  //        *
-  //        * IF we don't do this we will have two emissions when we refresh and keepValueOnRefresh = true.
-  //        */
-  //       if (index !== 0 && !config.keepValueOnRefresh && (state.isLoading || state.isRefreshing)) {
-  //         return null;
-  //       }
-  //       if (!state.isLoading || !state.isRefreshing) {
-  //         return state.value;
-  //       }
-  //     }),
-  //     filter((value) => value !== undefined)
-  //   ),
-  //   hasValue$: state$.pipe(map((state) => !!state.value)).pipe(distinctUntilChanged()),
-  //   isSuspense$: state$.pipe(map((state) => state.isLoading || state.isRefreshing)).pipe(distinctUntilChanged()),
-  //   hasError$: state$.pipe(map((state) => !!state.error)).pipe(distinctUntilChanged()),
-  //   error$: state$.pipe(map((state) => state.error)),
-  //   context$: state$.pipe(map((state) => state.context)),
-  //   state$: state$.pipe(
-  //     map((state) => ({
-  //       value: state.value,
-  //       hasValue: !!state.value,
-  //       hasError: !!state.error,
-  //       error: state.error,
-  //       isSuspense: state.isLoading || state.isRefreshing,
-  //       context: state.context,
-  //     })),
-  //     distinctUntilChanged()
-  //   ),
-  // };
 }
